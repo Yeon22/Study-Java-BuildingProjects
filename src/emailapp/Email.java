ed8385f9052e225eecdcf2fe66137c77f25bab15
yeon <bsuyeon16@gmail.com>
2018-10-02 22:02:17 +0900

    set variables

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,22 @@
 package emailapp;
 
 public class Email {
-
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	// Constructor to receive the first name and last name
+	
+	// Ask for the department
+	
+	// Generate a random password
+	
+	// Set the mailbox(메일함) capacity(용량)
+	
+	// Set the alternate email(대체 메일)
+	
+	// Change the password
 }
