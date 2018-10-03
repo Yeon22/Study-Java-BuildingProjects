6cfcb52f17c5577d29833189e4c06b28fc471415
yeon <bsuyeon16@gmail.com>
2018-10-03 17:35:24 +0900

    finish email application

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -17,11 +17,9 @@ public class Email {
 	public Email(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 		
 		// Call a method asking for the department return the department
 		this.department = setDepartment();
-		System.out.println("Department: " + this.department);
 		
 		// Call a method that return a random password
 		this.password = randomPassword(defaultPasswordLength);
@@ -29,12 +27,11 @@ public class Email {
 		
 		// Combine elements to generate email
 		this.email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + this.department + "." + this.companySuffix;
-		System.out.println("Your email is: " + this.email);
 	}
 	
 	// Ask for the department
 	private String setDepartment() {
-		System.out.print("DEPARTMENT CODES\n"
+		System.out.print("New Worker: " + firstName + ". \nDepartment Codes:\n"
 				+ "1. Sales\n"
 				+ "2. Development\n"
 				+ "3. Accounting\n"
@@ -79,4 +76,10 @@ public class Email {
 	public int getMailboxCapacity() { return mailboxCapacity; }
 	public String getAlternateEmail() { return alternateEmail; }
 	public String getPassword() { return password; }
+	
+	public String showInfo() {
+		return "DISPLAY NAME: " + firstName + " " + lastName + 
+				"\nCOMPANY EMAIL: " + email + 
+				"\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+	}
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,9 +4,7 @@ public class EmailApp {
 
 	public static void main(String[] args) {
 		Email eml = new Email("John", "Smith");
-		
-		eml.setAlternateEmail("js@gmail.com");
-		System.out.println("Your alternate email is: " + eml.getAlternateEmail());
+		System.out.println(eml.showInfo());
 	}
 
 }
