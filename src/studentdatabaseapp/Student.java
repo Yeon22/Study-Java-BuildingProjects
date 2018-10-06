b9e11274082724ad1ec0b64d82cea0a344345406
yeon <bsuyeon16@gmail.com>
2018-10-06 20:35:56 +0900

    Generate student ID

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,27 +6,36 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
-	private int studentID;
+	private String studentID;
 	private String courses;
 	private int tuitionBalance;
-	private int costOfCourse = 600;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
 	
 	// Constructor: prompt user to enter student's name and year
 	public Student() {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter student first name: ");
-		this.firstName = in.nextLine();
+		firstName = in.nextLine();
 		
 		System.out.print("Enter student last name: ");
-		this.lastName = in.nextLine();
+		lastName = in.nextLine();
 		
 		System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
-		this.gradeYear = in.nextInt();
+		gradeYear = in.nextInt();
+
+		setStudentID();
 		
-		System.out.println(this.firstName + " " + this.lastName + " " + this.gradeYear);
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+		
 	}
 	
 	// Generate an ID
+	private void setStudentID() {
+		// Grade Level + ID
+		id++;
+		studentID = gradeYear + "" + id;
+	}
 	
 	// Enroll in courses
 	
