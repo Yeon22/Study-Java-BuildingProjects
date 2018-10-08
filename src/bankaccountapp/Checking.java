58f46b9263e1be14abae7a8bc4a6e13c14e910c5
yeon <bsuyeon16@gmail.com>
2018-10-08 12:04:46 +0900

    Implementing the checking account debit card

--- src/bankaccountapp/Checking.java
+++ src/bankaccountapp/Checking.java
@@ -11,11 +11,20 @@ public class Checking extends Account {
 	public Checking(String name, String sSN, double initDeposit) {
 		super(name, sSN, initDeposit);
 		accountNumber = "2" + accountNumber;
+		setDebitCard();
 	}
 	
 	// List any methods apecific to the checking account
+	private void setDebitCard() {
+		debitCardNumber = (int)(Math.random() * Math.pow(10, 12));
+		debitCardPIN = (int)(Math.random() * Math.pow(10, 4));
+	}
+	
 	public void showInfo() {
 		super.showInfo();
-		System.out.println("ACCOUNT TYPE: Checking");
+		System.out.println("Your Checking Account Features" + 
+				"\n Debit Card Number: " + debitCardNumber + 
+				"\n Debit Card PIN: " + debitCardPIN
+				);
 	}
 }
