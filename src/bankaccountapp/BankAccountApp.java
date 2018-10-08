4ef74089656dad8bcde348fc918db7d7eeb08f92
yeon <bsuyeon16@gmail.com>
2018-10-08 10:59:03 +0900

    modify super constructor

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -3,9 +3,9 @@ package bankaccountapp;
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-		Checking chkacc1 = new Checking("Tom Wilson");
+		Checking chkacc1 = new Checking("Tom Wilson", "321456789", 1500);
 		
-		Savings savacc1 = new Savings("Rich Lowe");
+		Savings savacc1 = new Savings("Rich Lowe", "456657897", 2500);
 		
 		// Read a CSV file then create new accounts based on that data
 	}
